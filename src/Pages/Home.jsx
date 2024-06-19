fb4904b98789f564db05bdedce560dcddcdd61a2
Sarkies <Ahmed.Sarkes@gmail.com>
2024-06-19 23:33:29 +0300

    Update Home.jsx Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -76,7 +76,7 @@ const Home = () => {
                   animate={"visible"}
                   transition={{ delay: 0.38 }}
                   exit={{ x: "-100%", opacity: 0 }}
-                  href="https://drive.google.com/file/d/1J4zIkkHWFWWhheJPmeMpaglEJ0j1tDTu/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1DxcOXf5sFqfv2pSPGyHF9AEBxq9xQ9C4/view?usp=sharing"
                   target="_blank"
                 >
                   <button className="button rounded" type="button">
